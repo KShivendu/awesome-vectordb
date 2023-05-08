7cff3806c9b2f7d49d0c67d2177ddc6648037fe3
Aditya Vikram Thoomati <adivik2000@hotmail.com>
2023-05-08 22:30:23 +0530

    fastapi and batching

--- pine_wiki.py
+++ pine_wiki.py
@@ -1,18 +1,26 @@
+import math
 import os
+from typing import List
 
 import cohere
 import pinecone
 from datasets import load_dataset
-from typing import List
+from fastapi import FastAPI
+from loguru import logger
 
 
 class VectorDB:
     def __init__(self, index_name):
         self.index_name = index_name
+        logger.info(f"Index name: {self.index_name} initialized")
         # Load the dataset
-        self.dataset = load_dataset(
+        large_dataset = load_dataset(
             "Cohere/wikipedia-22-12-simple-embeddings", split="train"
         )
+        # We will only use the first 1000 records
+        self.dataset = large_dataset[0:200]
+
+        logger.info(f"Dataset loaded with {len(self.dataset)} records")
 
 
 class PineconeDB(VectorDB):
@@ -24,31 +32,41 @@ class PineconeDB(VectorDB):
         )
         # Create the index if it doesn't exist
         if self.index_name not in pinecone.list_indexes():
+            logger.info(f"Creating index: {self.index_name}")
             pinecone.create_index(index_name, dimension=768, metric="cosine")
 
         # Connect to the index
         self.index = pinecone.Index(index_name=index_name)
 
     def upsert(self) -> str:
-        self.vectors = [
-            (
-                f"{self.dataset[i]['id']}",
-                self.dataset[i]["emb"],
-                {"text": self.dataset[i]["text"]},
+        batch_size = 100  # Adjust the batch size as per your requirements
+        logger.info(f"total vectors from upsert: {len(self.dataset)}")
+        num_vectors = len(self.dataset)
+        logger.info(f"total num of vectors from upsert: {num_vectors}")
+        num_batches = math.ceil(num_vectors / batch_size)
+
+        logger.info(f"Upserting {num_vectors} vectors in {num_batches} batches")
+
+        for i in range(num_batches):
+            start_idx = i * batch_size
+            end_idx = min((i + 1) * batch_size, num_vectors)
+
+            vectors_batch = [
+                (
+                    f"{self.dataset[j]['id']}",
+                    self.dataset[j]["emb"],
+                    {"text": self.dataset[j]["text"]},
+                )
+                for j in range(start_idx, end_idx)
+            ]
+
+            logger.info(
+                f"Upserting batch {i + 1} of {num_batches}, from {start_idx} to {end_idx}"
             )
-            for i in range(len(self.dataset))
-        ]
 
-        # Note: When upserting larger amounts of data, upsert data in batches
-        # of 100 vectors or fewer over multiple upsert requests.
+            self.index.upsert(vectors_batch)
 
-        # Upsert the vectors in batches of 50
-        batch_size = 50
-        num_vectors = len(self.vectors)
-
-        for i in range(0, num_vectors, batch_size):
-            batch = self.vectors[i : i + batch_size]
-            self.index.upsert(batch)
+        logger.info(f"Upserted {num_vectors} vectors")
 
         return "Upserted successfully"
 
@@ -62,24 +80,27 @@ class PineconeDB(VectorDB):
 
 
 # Main code
-if __name__ == "__main__":
-    # Step 1: Create an index in Pinecone
-    # Define the index name
-    index_name = "wikipedia-embeddings"
+app = FastAPI()
+index_name = "wikipedia-embeddings"
+pinecone_obj = PineconeDB(index_name)
 
+
+@app.post("/upsert")
+def db_upsert():
+    if pinecone_obj.upsert() == "Upserted successfully":
+        return "Upserted successfully"
+    return "Upsert failed"
+
+
+@app.post("/query")
+def db_query(query: str):
+    logger.info(f"Query: {query}")
     co = cohere.Client(os.environ["COHERE_API_KEY"])
-
     # get the embeddings
-    query = "What is Old Testament?"
     query_embeds = co.embed([query], model="multilingual-22-12").embeddings
 
-    # Initialize the PineconeDB
-    pinecone_obj = PineconeDB(index_name)
-    pinecone_obj.upsert()
+    return pinecone_obj.query(query_embedding=query_embeds[0])
 
-    result = pinecone_obj.query(query_embedding=query_embeds[0])
-
-    print(result)
     # Output:
     # {
     #     "matches": [
@@ -94,3 +115,7 @@ if __name__ == "__main__":
     #     ],
     #     "namespace": "",
     # }
+
+
+if __name__ == "__main__":
+    app.run()
